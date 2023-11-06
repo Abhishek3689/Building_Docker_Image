1e808f8725e750c0037307d5d180f59063e95a8d
Abhishek3689 <abhisek.nishad3689@gmail.com>
2023-11-06 20:09:19 +0530

    style updated

--- app.py
+++ app.py
@@ -8,18 +8,21 @@ def home_page():
 
 @app.route('/calculate',methods=['POST'])
 def calculator():
-    if request.method == 'POST':
-        num1=float(request.form['num1'])
-        num2=float(request.form['num2'])
-        operation=request.form.get('submit')
-        result = None
-        
-        if 'add' in request.form:
-            result = num1 + num2
-            
-        elif 'subtract' in request.form:
-            result = num1 - num2
-        return render_template('home.html',result=result)
+    if request.method=='POST':
+        ops=request.form['operation']
+        num1=int(request.form['num1'])
+        num2=int(request.form['num2'])
+
+        if ops=='add':
+            result=num1+num2
+        if ops=='subtract':
+            result=num1-num2
+        if ops=='multiply':
+            result=num1*num2
+        if ops=='division':
+            result=num1/num2 
+    return render_template('home.html',result=result)
+    #return render_template('home.html',result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=8080)
